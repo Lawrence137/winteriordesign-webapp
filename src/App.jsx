663df9357831a3ef32f616be73aa6973e9b089a5
Lawrence Kimani <lawrencekimani18@gmail.com>
2025-03-31 21:51:29 +0300

    vercel analytics fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import './App.css';
 import './index.css';
 import Header from './components/Header';
